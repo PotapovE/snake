0a262e9528000531005c8034dfea71dae0de45e4
PotapovE <potapovomsk@gmail.com>
2022-04-15 22:18:56 +0300

    Небольшое ознакомление

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x1 = 1;
+int y1 = 3;
+char sym1 = '*';
+
+void Draw(int x, int y, char sym)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(sym);
+}
+
+Draw(x1, y1, sym1);
+
 Console.ReadLine();
